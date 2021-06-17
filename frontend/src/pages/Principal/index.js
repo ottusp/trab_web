498b569036ac8e3ddb14cbd6cc851f7368dccd33
Larissa Freire <48482909+LariFreire@users.noreply.github.com>
2021-06-17 15:04:24 -0300

    Adiciona component header

---
+++ frontend/src/pages/Principal/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import Header from '../../components/Header/Header';
+
+export default function Principal(){
+    return (
+        <div className="container">
+            <Header />
+        </div>
+    );
+}
--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -3,12 +3,14 @@ import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import AdminDashboard from './pages/AdminDashboard/index';
+import Principal from './pages/Principal/index';
 
 export default function Routes() {
     return (
         <BrowserRouter>
             <Switch>
-                <Route path="/" exact component={AdminDashboard} />
+                <Route path="/" exact component={Principal} />
+                <Route path="/admin/home" exact component={AdminDashboard} />
             </Switch>
         </BrowserRouter>
     );
