b2e8f92dca57c97b07ab81f79708f31b644371df
Larissa Freire <48482909+LariFreire@users.noreply.github.com>
2021-06-27 21:27:26 -0300

    adiciona pagina principal e de pesquisa

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -4,6 +4,8 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import AdminDashboard from './pages/AdminDashboard/index';
 import Principal from './pages/Principal/index';
+import Product from './pages/Product/index';
+import Search from './pages/Search/index';
 
 export default function Routes() {
     return (
@@ -11,6 +13,8 @@ export default function Routes() {
             <Switch>
                 <Route path="/" exact component={Principal} />
                 <Route path="/admin/home" exact component={AdminDashboard} />
+                <Route path="/product" exact component={Product} />
+                <Route path="/search" exact component={Search} />
             </Switch>
         </BrowserRouter>
     );
