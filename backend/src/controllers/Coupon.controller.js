d7b705aa52a9c7abc6a51e39d72f40b3877c5b64
Lucas <lucasmfromero@gmail.com>
2021-07-26 15:45:31 -0300

    feat: comentarios no back 2/2

--- backend/src/controllers/Coupon.controller.js
+++ backend/src/controllers/Coupon.controller.js
@@ -1,6 +1,7 @@
 const Coupon = require('../models/Coupon');
 
 module.exports = {
+    // creates a coupon in database
     async store(req, res) {
         const code = req.body.code;
         const uses = req.body.uses;
@@ -15,22 +16,27 @@ module.exports = {
         return res.status(200).end();
     },
 
+    // uses the coupon
     async check(req, res) {
         const code = req.body.code;
 
         try {
-            var coupon = await Coupon.find({ code });
+            var coupon = await Coupon.find({ code });   // finds the coupon
         } catch (err) {
             console.log(err);
             return res.status(500).end();
         }
 
-        if (coupon.uses > 0) {
-            coupon.uses -= 1;
+        if (!coupon) {
+            return res.status(404).end();
+        }
+
+        if (coupon.uses > 0) {  //if the coupon can be used
+            coupon.uses -= 1;   //uses it
             
-            if (coupon.uses == 0) {
+            if (coupon.uses == 0) { //if it has no more uses left
                 try {
-                    var coupon = await Coupon.delete({ code });
+                    var coupon = await Coupon.delete({ code }); //deletes the coupon
                 } catch (err) {
                     console.log(err);
                     return res.status(500).end();
@@ -38,7 +44,7 @@ module.exports = {
             }
             else {
                 try {
-                    await coupon.save();
+                    await coupon.save();    //saves the changes
                 } catch (err) {
                     console.log(err);
                     return res.status(500).end();
